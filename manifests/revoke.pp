1e288ec1929e0f0e902166e5a182e1713bdd4140
Pedro González Serrano <niteman.es@gmail.com>
2020-05-05 18:28:12 +0200

    Fix schedule name

--- manifests/revoke.pp
+++ manifests/revoke.pp
@@ -48,6 +48,6 @@ define openvpn::revoke (
     command  => $renew_command,
     cwd      => "${openvpn::server_directory}/${name}/easy-rsa",
     provider => 'shell',
-    schedule => "renew crl.pem schedule on ${name}",
+    schedule => "renew crl.pem schedule on ${server}",
   }
 }
